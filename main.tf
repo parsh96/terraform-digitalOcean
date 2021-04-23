6759ff492201da0bb27d2880c18df29d112c101a
parsh96 <shah.parshva@ymail.com>
2021-04-23 16:35:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,11 @@ terraform {
   }
 }
 
+variable "do_token" {}
 
+provider "digitalocean" {
+  token = var.do_token
+}
 resource "digitalocean_droplet" "web" {
   name   = "web-1"
   size   = "s-1vcpu-1gb"
